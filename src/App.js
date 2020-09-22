c93ec2c0aa37844666da5f21ece1a0d3e7f415ca
DenSpar <57890463+DenSpar@users.noreply.github.com>
2020-09-22 18:44:05 +0400

    сделал заполнение стейта

--- src/App.js
+++ src/App.js
@@ -1,91 +1,28 @@
 import React, { useState } from 'react';
 import './App.css';
 import Table from 'components/table/Table';
+import getList from 'js/getList';
 
-var api = require('twitch-api-v5');
-
-api.clientID = '08i240lntql615wx8iozx8rq23krxr';
-
-let strSilverName = {
-  id: 70075625,
-  name: "SilverName",
-  logo: "https://static-cdn.jtvnw.net/jtv_user_pictures/55d2c6d9-9d70-40e2-b30b-773ea9b19cd6-profile_image-300x300.png",
-  onAir: false,
-  videosNum: 77,
-  followers: 123456
-};
-let strAsmadey = {
-  id: 83597658,
-  name: "Asmadey",
-  logo: "https://static-cdn.jtvnw.net/jtv_user_pictures/c0f1ee2c-1d3c-42c9-b963-cbdba2728d1f-profile_image-300x300.png",
-  onAir: false,
-  videosNum: 55,
-  followers: 780012
-};
-let streamersArrTest = [strSilverName,strAsmadey];
-
-//пример
-// api.users.userByID({ userID: '12826' }, (err, res) => {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log('test example - userByID', res);
-//     /* Example response
-//       {
-//         display_name: 'Twitch',
-//         _id: '12826',
-//         name: 'twitch',
-//         type: 'user',
-//         ...
-//       }
-//     */
-//   }
-// });
-
-//поиск каналов по имени
-// api.search.channels({ query: 'Asmadey' }, (err, res) => {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log('test example - search.channel', res);
-//   }
-// });
-
-// //поиск канала по номеру
-// api.channels.channelByID({ channelID: '70075625' }, (err, res) => {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log('test example - channels.channelByID', res);
-//   }
-// });
-
-// //проверка, если канал(по номеру) сейчас стримит
-// api.streams.channel({ channelID: '70075625' }, (err, res) => {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log('test example - streams.channel', res);
-//   }
-// });
-
-// //найти видео канала(по номеру)
-// api.channels.videos({ channelID: '70075625', limit:10 }, (err, res) => {
-//   if(err) {
-//     console.log(err);
-//   } else {
-//     console.log('test example - channels.videos', res);
-//   }
-// });
+let channelsArr = [70075625, 83597658, 142022455];
 
 function App() {
-  const [tableState, setTableState] = useState(streamersArrTest);
+  const [tableState, setTableState] = useState([]);
+  if (tableState.length === 0) {
+    getList(channelsArr)
+    .then(dataArr => {console.log(dataArr);setTableState(dataArr)});
+    return (
+      <div className="App">
+        <h1 className="serviceName">twitch-analytics</h1>
+        <p>loading...</p>
+      </div>
+    )
+  };
   return (
     <div className="App">
-      <h1>twitch-analytics</h1>
+      <h1 className="serviceName">twitch-analytics</h1>
       <Table streamers={tableState} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,27 +1,38 @@
 import React from 'react';
 import './table.css';
 
+//если не стримит - onAir: {stream: null}
+
+const OnAir = ({stream}) => {
+  if (stream) {
+    return(
+      <span className="onAir" >В ЭФИРЕ</span>
+    )
+  } else {return null}
+};
+
 const Table = ({streamers}) => {
     console.log('table begin');
     return(
       <table className="table">
         <thead>
           <tr>
-            <th className="table_cell">name/logo</th>
-            <th className="table_cell">num videos</th>
+            <th className="table_cell">logo/name</th>
+            <th className="table_cell">videos</th>
             <th className="table_cell">followers</th>
           </tr>
         </thead>
         <tbody>
-          {streamers.map((streamer) => (
-            <tr>
+          {streamers.map((streamer, num) => (
+            <tr key={num}>
               <td className="table_cell">
                 <div className="flex">
-                  <img className="table_img" src={streamer.logo} />
+                  <img className="table_img" src={streamer.logo} alt={streamer.name}/>
                   <p>{streamer.name}</p>
-                </div>                
+                  <OnAir stream={streamer.stream}/>
+                </div>
               </td>
-              <td className="table_cell">{streamer.videosNum}</td>
+              <td className="table_cell">{streamer.totalVideos}</td>
               <td className="table_cell">{streamer.followers}</td>
             </tr>
           ))}
