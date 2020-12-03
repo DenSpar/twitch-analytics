bf7ddf516c042502dbdfcbfd7aa6cc42442f8d7b
DenSpar <57890463+DenSpar@users.noreply.github.com>
2020-12-03 17:29:07 +0300

    функция для проверки если у удаляемого стримера идет стрим

--- back_end/apiHandlers/totalDeleteStreamer.js
+++ back_end/apiHandlers/totalDeleteStreamer.js
@@ -1,5 +1,23 @@
 const deleteStreamerFromDB = require('../collectionStreamers/deleteStreamerFromDB.js');
 const deleteStreamerStats = require('../collectionStats/deleteStreamerStats.js');
+const findLiveStream = require('../collectionLiveStreams/findLiveStream.js');
+const getStreamsChannelById = require('../twitchApiRequests/getStreamsChannelById.js');
+
+let checkNowStream = (streamerID) => {
+    return new Promise (function (resolve, reject) {
+        findLiveStream(streamerID)
+        .then(liveStreamInDB => {
+            if (liveStreamInDB) {
+                getStreamsChannelById(streamerID)
+                .then(stream => {
+                    if(stream) {
+                        resolve(stream); // сделать разные ответы для двух кейсов
+                    } else { resolve(stream); } // сделать разные ответы для двух кейсов
+                });
+            } else { resolve(liveStreamInDB); };
+        })
+    })
+};
 
 module.exports = function totalDeleteStreamer(streamerID) {
     return new Promise (function(resolve, reject) {
@@ -7,11 +25,13 @@ module.exports = function totalDeleteStreamer(streamerID) {
         Promise.all([
             deleteStreamerFromDB(streamerID), // удаление из основного списка
             deleteStreamerStats(streamerID), // удаление статы стримера
+            checkNowStream(streamerID), // проверка идет ли сейчас стрим на этом канала
         ])
         .then(res => {
             let answer = {
                 delStreamer: res[0],
-                delStats: res[1]
+                delStats: res[1],
+                nowStream: res[2]
             };
             resolve(answer);
         })
