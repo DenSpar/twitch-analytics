d957baeb817fbb4e10f64335c667a28bf09f0b5e
DenSpar <57890463+DenSpar@users.noreply.github.com>
2020-12-10 12:02:15 +0300

    fix myLogger

--- back_end/myLogger.js
+++ back_end/myLogger.js
@@ -6,18 +6,22 @@ let getLogDate = () => {
     let timeStr = date.toLocaleTimeString('ru-Ru', { timeZone: 'Europe/Moscow' });
     let milSecs = date.getMilliseconds();
     let fullDateStr = dateStr + ' ' + timeStr + '.' + milSecs;
-    alert (fullDateStr);
+    return fullDateStr;
 };
 
 module.exports = function myLogger (logText, logLevel = 'TRACE') {
     console.log(logText);
     let logDate = getLogDate ();
     let logFull = logDate + ' ' + logLevel + ': ' + logText;
-    fs.appendFile('../metacorpstat.log', logFull + '/n');
+    fs.appendFile('../metacorpstat.log', `\n${logFull}`, 'utf8', (err) => {
+        if (err) {
+          console.log('че то пошло не так');
+        };
+    });
 };
 
 // ERROR — приложение в критическом положении, требуется внимание человека для продолжения. Появляется довольно редко, но метко. Я использую его для очень низкоуровневых вещей или для необработанных исключений
 // WARN — произошло что-то необычное, выбивающееся из обычного сценария, но приложение умное и восстановило свою работу само. Я использую этот уровень в обрабочиках ошибок.
 // INFO — что сейчас происходит
 // DEBUG — что сейчас происходит, более подробно
-// TRACE — пишем как в твиттер, все что не попадя.
+// TRACE — пишем как в твиттер, все что не попадя
